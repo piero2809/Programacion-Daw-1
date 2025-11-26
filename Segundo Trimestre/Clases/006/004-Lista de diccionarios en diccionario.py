c96969f9320e74b199f38c0ef2eee51771f92a95
Piero Funes <pierofl2005@gmail.com>
2025-11-26 09:20:39 +0100

    1

---
+++ Segundo Trimestre/Clases/006/004-Lista de diccionarios en diccionario.py
@@ -0,0 +1,18 @@
+
+persona = {
+    "nombre":"Piero Gabriel",
+  "apellidos":"Funes Larios",
+  "correo":"Piero@jocarsa.com",
+  "edad":20,
+  "telefonos":[
+      {
+          "tipo":"fijo",
+                "numero": 96123455
+      },{
+       "tipo":"movil",
+            "numero": 12345678   
+      }
+  ]
+}
+
+print (persona)
