e15edc974a4b3f60a1d09ed837da7a43af3301e6
Piero Funes <pierofl2005@gmail.com>
2025-12-05 08:51:08 +0100

    1

--- Segundo Trimestre/Clases/011-Utilizacion avanzada de clases/composicion de clases/004-super clase.py
+++ Segundo Trimestre/Clases/011-Utilizacion avanzada de clases/composicion de clases/004-super clase.py
@@ -1,4 +1,4 @@
-class Profesor():
+class Persona():
   def __init__(self,nombre,apellidos,email):
     self.nombre = nombre
     self.apellidos = apellidos
@@ -6,15 +6,16 @@ class Profesor():
   def dameDatos(self):
     return self.nombre+self.apellidos
 
-class Alumno():
+class Profesor(Persona):
   def __init__(self,nombre,apellidos,email):
-    self.nombre = nombre
-    self.apellidos = apellidos
-    self.email = email
-  def dameDatos(self):
-    return self.nombre+self.apellidos
+  	super() 
+  
+class Alumno(Persona):
+  def __init__(self,nombre,apellidos,email):
+    super()
+  
     
-alumno1 = Alumno("Piero Gabriel","Funes","Piero@gmail.com")
+alumno1 = Alumno("Jose Vicente","Carratala","info@jocarsa.com")
 print(alumno1.dameDatos())
 
 profesor1 = Profesor("Juan","Garcia","juan@jocarsa.com")
