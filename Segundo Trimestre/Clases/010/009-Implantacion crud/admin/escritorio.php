f705acbc15aa7e9a69c2e7894715a62efbc93b60
Piero Funes <pierofl2005@gmail.com>
2025-12-11 10:56:14 +0100

    1

---
+++ Segundo Trimestre/Clases/010/009-Implantacion crud/admin/escritorio.php
@@ -0,0 +1,22 @@
+<!--http://..../basesdedatosdamdaw2526/008-Proyectos/003-Panel%20de%20control/101-Ejercicios/aplicacion/admin/escritorio.php -->
+
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>El jocarsa - Panel de control</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <nav>
+        <button>Noticias</button>
+        <button>Autores</button>
+    </nav>
+    <main>
+        <?php include "inc/read/leer.php"; ?>
+    </main>
+</body>
+
+</html>
