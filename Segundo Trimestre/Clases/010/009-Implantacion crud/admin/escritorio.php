c3a94a5ec61a8321c0a04206c8d8d12ffdf2c334
Piero Funes <pierofl2005@gmail.com>
2025-12-12 09:31:21 +0100

    1

--- Segundo Trimestre/Clases/010/009-Implantacion crud/admin/escritorio.php
+++ Segundo Trimestre/Clases/010/009-Implantacion crud/admin/escritorio.php
@@ -2,27 +2,35 @@
 
 <!doctype html>
 <html lang="es">
-	<head>
-  	<title>El jocarsa - Panel de control</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="css/estilo.css">
-  </head>
-  <body>
-  	<nav>
-    	<button>Noticias</button>
-      <button>Autores</button>
-    </nav>
-    <main>
-    	<?php
-      	if(isset($_GET['accion'])){
-        	if($_GET['accion'] == "nuevo"){
-          	include "inc/create/formulario.php";
-          }
-        }else{
-      		include "inc/read/leer.php"; 
-        }
-      ?>
-      <a href="?accion=nuevo" id="nuevo">+</a>
-    </main>
-  </body>
+
+<head>
+  <title>El jocarsa - Panel de control</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+  <nav>
+    <button>Noticias</button>
+    <button>Autores</button>
+  </nav>
+  <main>
+    <?php
+    // Esto se conoce como router (enrutador) /////////////
+    if (isset($_GET['accion'])) {
+      if ($_GET['accion'] == "nuevo") {
+        include "inc/create/formulario.php";
+      } else if ($_GET['accion'] == "eliminar") { 					// Defino la acción eliminar
+        include "inc/delete/eliminar.php";							// En ese caso incluyo eliminar.php
+      } else if ($_GET['accion'] == "editar") { 						// Defino la acción editar
+        include "inc/update/formularioactualizar.php";	// En ese caso incluyo el formulario de la edicion.php
+      }
+    } else {
+      include "inc/read/leer.php";
+    }
+    ?>
+    <a href="?accion=nuevo" id="nuevo">+</a>
+  </main>
+</body>
+
 </html>
---
+++ Segundo Trimestre/Clases/010/009-Implantacion crud/admin/inc/update/formularioactualizar.php
@@ -0,0 +1,48 @@
+<?php
+
+// Primero traemos el formulario de insertar
+// Y luego lo rellenamos de datos (los datos a actualizar)
+
+$host = "localhost";
+$user = "periodico";
+$pass = "Periodico123$";
+$db = "periodico";
+
+$conexion = new mysqli($host, $user, $pass, $db);
+
+$sql = "SELECT * FROM noticias WHERE id = " . $_GET['id'] . ";";
+
+$resultado = $conexion->query($sql);
+while ($fila = $resultado->fetch_assoc()) {
+    ?>
+
+    <form action="inc/update/procesaformulario.php" method="POST">
+        <div class="controlformulario">
+            <label for="titulo">Título de la nueva noticia</label>
+            <input type="text" name="titulo" id="titulo" value="<?= $fila['titulo'] ?>">
+        </div>
+
+        <div class="controlformulario">
+            <label for="contenido">Contenido de la nueva noticia</label>
+            <textarea id="contenido" name="contenido"><?= $fila['contenido'] ?></textarea>
+        </div>
+
+        <div class="controlformulario">
+            <label for="fecha_publicacion">Fecha de la nueva noticia</label>
+            <input type="text" name="fecha_publicacion" id="fecha_publicacion" value="<?= $fila['fecha_publicacion'] ?>">
+        </div>
+
+        <div class="controlformulario">
+            <label for="autor_id">Autor de la nueva noticia</label>
+            <input type="text" name="autor_id" id="autor_id" value="<?= $fila['autor_id'] ?>">
+        </div>
+
+        <input type="submit">
+
+    </form>
+
+    <?php
+
+}
+
+?>
