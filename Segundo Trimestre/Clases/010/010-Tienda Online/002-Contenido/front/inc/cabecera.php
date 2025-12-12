571a975ab391244368a4c8fb599cd6c5900e7f8a
Piero Funes <pierofl2005@gmail.com>
2025-12-12 10:21:34 +0100

    1

---
+++ Segundo Trimestre/Clases/010/010-Tienda Online/002-Contenido/front/inc/cabecera.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda Piero</title>
+</head>
+
+<body>
+    <header>
+        <h1>Tienda Piero</h1>
+        <h2>Tienda en clase</h2>
+        <nav>
+            <ul>
+                <li>
+                    <a href="catalogo.php">Catálogo</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
---
+++ Segundo Trimestre/Clases/010/010-Tienda Online/002-Contenido/front/index.php
@@ -0,0 +1,7 @@
+<?php include "inc/cabecera.php"; ?>
+<section id="Heroe">
+    <h3>Motivo por el cual deberia comprar</h3>
+    <p>Frase sugerente al respecto</p>
+    <a href="catalogo.php">Vamos a ver esa maravilla de catálogo</a>
+</section>
+<?php include "inc/piedepagina.php"; ?>
